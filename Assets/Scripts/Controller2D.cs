29596e654f2be9d0b0add6e59a3f30dc2ab69d6e
GuiMac <guibasconti@gmail.com>
2018-12-01 10:31:00 -0200

    Add player movement

---
+++ Assets/Scripts/Controller2D.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Controller2D : MonoBehaviour {
+
+	Rigidbody2D rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	public void Move(Vector2 velocity, Vector2 input) {
+		rb.AddForce(velocity * input);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controller2D))]
+public class Player : MonoBehaviour {
+
+	Controller2D controller;
+	Vector2 input;
+	public Vector2 velocity;
+
+	void Start() {
+		controller = GetComponent<Controller2D>();
+	}
+	
+	void Update () {
+		controller.Move(velocity * Time.deltaTime, input);
+	}
+
+	public void SetDirectionalInput(Vector2 directionalInput) {
+		input = directionalInput;
+	}
+}
