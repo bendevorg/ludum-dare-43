67166d308435ed50f786ba47cabcf063037e4a22
GuiMac <guibasconti@gmail.com>
2018-12-01 03:31:07 -0200

    Create rope mechanics

---
+++ Assets/Scripts/RopeController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeController : MonoBehaviour {
+
+	public GameObject ropeShooter;
+	private SpringJoint2D rope;
+	public int maxRopeFrameCount;
+	private int ropeFrameCount;
+
+	public LineRenderer lineRenderer;
+	public LayerMask layerMask;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			Fire();
+		} else if (Input.GetKeyDown(KeyCode.Space)) { 
+			DestroyRope();
+		}
+	}
+
+	void LateUpdate() {
+		if (rope) {
+			lineRenderer.enabled = true;
+			lineRenderer.SetVertexCount(2);
+			lineRenderer.SetPosition(0, ropeShooter.transform.position);
+			lineRenderer.SetPosition(1, rope.connectedAnchor);
+		} else {
+			lineRenderer.enabled = false;
+		}
+	}
+
+	void FixedUpdate() {
+		if (rope) {
+			ropeFrameCount++;
+			if (ropeFrameCount > maxRopeFrameCount) {
+				GameObject.DestroyImmediate(rope);
+				ropeFrameCount = 0;
+			}
+		}
+	}
+
+	void Fire() {
+		Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector3 position = ropeShooter.transform.position;
+		Vector3 direction = mousePosition - position;
+
+		RaycastHit2D hit = Physics2D.Raycast(position, direction, Mathf.Infinity, layerMask);
+
+		if (hit.collider) {
+			SpringJoint2D newRope = ropeShooter.AddComponent<SpringJoint2D>();
+			newRope.enableCollision = false;
+			newRope.frequency = 0f;
+			newRope.connectedAnchor = hit.point;
+			newRope.enabled = true;
+
+			GameObject.DestroyImmediate(rope);
+			rope = newRope;
+			ropeFrameCount = 0;
+		}
+	}
+
+	void DestroyRope() {
+		GameObject.DestroyImmediate(rope);
+		ropeFrameCount = 0;
+	}
+}
