2a45aa64d1d7c74eed4e648493b6f4eb8e7ffa42
Lucas Lazarini <lucalazarini@hotmail.com>
2018-12-02 10:52:23 -0200

    Add player death sound

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class Death : MonoBehaviour {
 
 	const string playerTag = "Player";
+	private AudioSource audioSource;
+	public AudioClip deathClip;
 
 	void OnTriggerEnter2D(Collider2D collider) {
 		if (collider.tag == playerTag) {
+
+			// collider.gameObject.SetActive(false);
+
+			audioSource = GetComponent<AudioSource>();
+
+			audioSource.PlayOneShot(deathClip, 1F);
+
 			Destroy(collider.gameObject);
 			GameController.gameController.PlayerDeath();
 		}
